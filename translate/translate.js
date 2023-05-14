c8decf81f1ce6593f28d53168c4e28bb3c4db0fb
kev04L <120223772+kev04L@users.noreply.github.com>
2023-05-13 22:50:58 -0600

    Add files via upload

---
+++ translate/translate.js
@@ -0,0 +1,44 @@
+function adjustInBoxHeight(el) {
+    el.style.height = "auto";
+    el.style.height = el.scrollHeight + "px";
+}
+
+const LETTER = "abcdefghijklmnopqrstuvwxyz"; //for getting correct letter position
+const PUNCTUATION = " .,!?";
+
+$( document ).ready(function() {
+    $( "#submit" ).click(function() {
+        $('#output').html("");
+        var intxt = $('#inBox').val().toLowerCase();
+        var outtxt = "";
+        var base = "m";
+        var bswap = false;
+        //cycle through all the letters in the input text
+        for (let i in intxt) {
+            if (LETTER.indexOf(intxt[i]) == -1) {
+                if (PUNCTUATION.indexOf(intxt[i]) != -1) {
+                    $("#output").append(intxt[i]);
+                }
+                if (intxt[i] == "~") {
+                    bswap = true;
+                }
+
+            }
+            else {
+                var unicode = 3400; 
+                if (bswap) {
+                    base = intxt[i];
+                    unicode += 27;
+                    bswap = false;
+                } else {
+                    unicode += LETTER.indexOf(intxt[i]) + 1; //getting correct letter
+                }
+                unicode += LETTER.indexOf(base) * 27; //setting correct base
+                var unistring = "0x" + unicode;
+                console.log(unistring);
+                $("#output").append(String.fromCodePoint(unistring));
+            }
+        }
+        //$("#inBox").html(outtxt);
+    });
+});
