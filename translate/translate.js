10413f17a3cb0b789d8304622ec6e8eadc7100a7
kev04L <120223772+kev04L@users.noreply.github.com>
2023-05-25 11:19:22 -0600

    Add files via upload

--- translate/translate.js
+++ translate/translate.js
@@ -1,48 +1,56 @@
-window.addEventListener('resize', adjustInBoxHeight);
-function adjustInBoxHeight() {
+//window.addEventListener('resize', adjustInBoxHeight);
+/*function adjustInBoxHeight() {
     //el.style.height = "auto";
     //el.style.height = el.scrollHeight + "px";
-    var inBoxHeight = document.getElementById('myTextarea');
+    var inBoxHeight = document.getElementById('inBox');
     inBoxHeight.style.height = textarea.scrollHeight;
 
-}
+}*/
+/*var textarea = document.getElementById("myTextarea");
+var height = textarea.scrollHeight;
+console.log("Textarea height:", height);*/
+/*function logTextareaHeight() {
+  var textarea = document.getElementById("myTextarea");
+  var height = textarea.scrollHeight;
 
+  console.log("Textarea height:", height);
+}*/
 const LETTER = "abcdefghijklmnopqrstuvwxyz"; //for getting correct letter position
 const PUNCTUATION = " .,!?";
 
-$( document ).ready(function() {
-    $( "#submit" ).click(function() {
-        $('#output').html("");
-        var intxt = $('#inBox').val().toLowerCase();
-        var outtxt = "";
-        var base = "m";
-        var bswap = false;
-        //cycle through all the letters in the input text
-        for (let i in intxt) {
-            if (LETTER.indexOf(intxt[i]) == -1) {
-                if (PUNCTUATION.indexOf(intxt[i]) != -1) {
-                    $("#output").append(intxt[i]);
-                }
-                if (intxt[i] == "~") {
-                    bswap = true;
-                }
+$(document).ready(function () {
+  $("#submit").click(function () {
+    $("#output").html("");
+    var intxt = $("#inBox").text().toLowerCase();
+    console.log(intxt);
 
-            }
-            else {
-                var unicode = 3400; 
-                if (bswap) {
-                    base = intxt[i];
-                    unicode += 27;
-                    bswap = false;
-                } else {
-                    unicode += LETTER.indexOf(intxt[i]) + 1; //getting correct letter
-                }
-                unicode += LETTER.indexOf(base) * 27; //setting correct base
-                var unistring = "0x" + unicode;
-                console.log(unistring);
-                $("#output").append(String.fromCodePoint(unistring));
-            }
+    var outtxt = "";
+    var base = "m";
+    var bswap = false;
+    //cycle through all the letters in the input text
+    for (let i in intxt) {
+      if (LETTER.indexOf(intxt[i]) == -1) {
+        if (PUNCTUATION.indexOf(intxt[i]) != -1) {
+          $("#output").append(intxt[i]);
         }
-        //$("#inBox").html(outtxt);
-    });
+        if (intxt[i] == "~") {
+          bswap = true;
+        }
+      } else {
+        var unicode = 3400;
+        if (bswap) {
+          base = intxt[i];
+          unicode += 27;
+          bswap = false;
+        } else {
+          unicode += LETTER.indexOf(intxt[i]) + 1; //getting correct letter
+        }
+        unicode += LETTER.indexOf(base) * 27; //setting correct base
+        var unistring = "0x" + unicode;
+        console.log(unistring);
+        $("#output").append(String.fromCodePoint(unistring));
+      }
+    }
+    //$("#inBox").html(outtxt);
+  });
 });
