6aafc84fc566a203c15e1c38914592e864f1e43e
kev04L <120223772+kev04L@users.noreply.github.com>
2023-05-30 17:22:32 -0600

    Add files via upload

--- english/english.js
+++ english/english.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
     var letterPosition = 0;
     var baseCode = 12;
     var basePosition = 20;
+    var font = "Roboto";
+    var unknownCharElement = "֐";
     for (let i in intxt) {
       unicodeValue = intxt.charCodeAt(i);
       if (unicodeValue > 1304 && unicodeValue < 1398) {
@@ -19,11 +21,21 @@ $(document).ready(function () {
           letterPosition = unicodeValue - basePosition - 1312;
         }
         letterCode = 5 * Math.floor(letterPosition / 9) + (letterPosition % 9);
-        //console.log("letterPosition " + letterPosition);
-        //console.log("baseCode: " + baseCode);
-        //console.log("unicodeValue: " + unicodeValue);
-        //console.log("basePosition: " + basePosition);
-        $("#output").append(LETTER[letterCode]);
+        if ((letterCode > -1 && letterCode < 26) || letterCode == 27) {
+          font = "Roboto";
+          var $out = $("<span>")
+            .text(LETTER[letterCode])
+            .css("font-family", font);
+          $("#output").append($out);
+          //$("#output").append(LETTER[letterCode]);
+        } else {
+          font = "Cipher 1.13";
+          var $out = $("<span>")
+            .text(unknownCharElement)
+            .css("font-family", font);
+          $("#output").append($out);
+          //$("#output").append("֐");
+        }
       } else if (unicodeValue > 1397 && unicodeValue < 1424) {
         if (unicodeValue == 1423) {
           baseCode = unicodeValue - 1398 + 2; //accounts for the switch z being centered
@@ -31,7 +43,8 @@ $(document).ready(function () {
           baseCode = unicodeValue - 1398;
         }
         basePosition = 40 - (9 * Math.floor(baseCode / 5) + (baseCode % 5));
-        //console.log("Base switch to: Base " + LETTER[baseCode]);
+      } else if (intxt[i] == "\n") {
+        $("#output").append("<br>");
       } else {
         $("#output").append(intxt[i]);
       }
